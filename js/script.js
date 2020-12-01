676f395fc3b91799ae2b5a46139b5611e530e06c
KPGuerra <KPGuerra96@gmail.com>
2020-12-01 17:58:32 -0500

    Mini MVP

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 let currentUser = 0
 let currentHometown = 0
 let currentUserId = 0
+let currentUserObj = {}
 
 
 // DOM QUERIES 
@@ -10,6 +11,7 @@ const landmarkBar = document.querySelector(".landmark-bar")
 const loginBtn = document.querySelector("#login-button")
 const hometownDiv = document.querySelector(".hometown-bar")
 const landmarkForm = document.querySelector(".landmark-form")
+const landmarkInfo = document.querySelector(".landmark-info")
 
 const signInBar2 = document.querySelector(".sign-in-2")
 const signInBar = document.querySelector(".sign-in-bar")
@@ -33,11 +35,20 @@ function fetchUsers() {
 //         console.log(userObj)
 //     })
 
-function fetchUserLandmarks() {
-    fetch(`http://localhost:3000/api/v1/users/${currentUserId}`)
+function fetchUserLandmarks(user) {
+    fetch(`http://localhost:3000/api/v1/users/${user}`)
     .then(resp => resp.json())
-    .then(landmarksArray => {
-        renderLandmarks(landmarksArray)
+    .then(userObj => {
+        currentUserObj = userObj
+        renderLandmarks(userObj.landmarks)
+    })
+}
+
+function fetchAllLandmarks () {
+    fetch(`http://localhost:3000/api/v1/landmarks`)
+    .then(resp => resp.json())
+    .then(landmarkArr => {
+        renderLandmarks(landmarkArr)
     })
 }
 
@@ -53,21 +64,66 @@ function renderLandmarks(landmarksArray) {
     landmarksArray.forEach(landmark => {
       const li = document.createElement("li")
       li.textContent = landmark.name
+      li.dataset.id = landmark.id
       landmarkBar.append(li)
     })
 }
+
+function renderLandmarkInfo (id) {
+    landmarkInfo.hidden = false
+    const name = document.querySelector("#landmark-name")
+    const address = document.querySelector("#landmark-address")
+    const bio = document.querySelector("#landmark-bio")
+    const genre = document.querySelector("#landmark-genre")
+
+    fetch(`http://localhost:3000/api/v1/landmarks/${id}`)
+    .then(resp => resp.json())
+    .then(landmarkObj => {
+        name.textContent = landmarkObj.name
+        address.textContent = landmarkObj.address
+        bio.textContent = landmarkObj.bio
+        genre.textContent = landmarkObj.genre
+    })
+
+}
+
 // EVENT LISTENERS
+landmarkBar.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        // debugger
+        renderLandmarkInfo(parseInt(e.target.dataset.id))
+    }
+})
+
 landmarkForm.addEventListener("submit", function(e) {
     e.preventDefault()
-    debugger
     const newLandmark = {
+        user_id: parseInt(currentUserId),
         name: e.target.name.value,
         address: e.target.address.value,
         img_url: e.target.image.value,
         bio: e.target.bio.value,
         genre: e.target.genre.value
     }
+    // debugger
+    fetch("http://localhost:3000/api/v1/landmarks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newLandmark)
+    })
+    .then(resp => resp.json())
+    .then(newLandmark => {
+        // renderLandmarks(newLandmark)
+        console.log(newLandmark)
+    })
+    .catch(error => console.log(error))
+    // debuggers
+    fetchUserLandmarks(currentUserId)
+
 })
+
 signInBar2.addEventListener("submit", function (e) {
     e.preventDefault()
     const imageInput = document.querySelector("#image-input")
@@ -103,4 +159,5 @@ signInBar2.addEventListener("submit", function (e) {
 
 // INITIALIZER
 fetchUsers()
-fetchUserLandmarks()
+// fetchUserLandmarks()
+// fetchAllLandmarks()
