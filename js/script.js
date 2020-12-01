995db731b070cf05e9ed474ed25aa12eae66ffa6
StevenWuTG <stevenwu91@icloud.com>
2020-12-01 12:05:08 -0500

    frontend 15%

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+// gettho session state 
+
+let currentUser = 0
+let currentHometown = 0
+
 
 // DOM QUERIES 
 const landmarkBar = document.querySelector(".landmark-bar")
 const loginBtn = document.querySelector("#login-button")
+const hometownDiv = document.querySelector(".hometown-bar")
 
+const signInBar2 = document.querySelector(".sign-in-2")
+const signInBar = document.querySelector(".sign-in-bar")
+const loginInput = document.querySelector("#login-input")
+const hometownInput = document.querySelector("#hometown-input")
 // DOM SLAPPAGE
 
 // FETCHES
@@ -25,15 +35,19 @@ fetch("http://localhost:3000/api/v1/users/1/")
 
 // EVENT LISTENERS
 
-loginBtn.addEventListener("click", function(e){
+signInBar2.addEventListener("submit", function(e){
     e.preventDefault()  
-
-    const signInBar = document.querySelector(".sign-in-bar")
-    const signInBar2 = document.querySelector(".sign-in-2")
-    signInBar.classList.remove("black-background")
-    signInBar2.innerHTML = ""
+    
     // debugger
-    console.log("click")
+    signInBar.hidden = true
+    signInBar2.hidden = true
+    currentUser = loginInput.value
+    currentHometown = hometownInput.value
+    hometownDiv.innerHTML = `<h3> ${currentHometown}</h3>`
+    alert(`WELCOME ${currentUser}`)
+    // signInBar.classList.remove("black-background")
+    // signInBar2.innerHTML = ""
+    
 })
 
 // INITIALIZER
