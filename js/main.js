03dca830db327599e8fc091cab4131961e2f25ea
KPGuerra <KPGuerra96@gmail.com>
2020-12-06 00:32:55 -0500

    Added more data

--- js/main.js
+++ js/main.js
@@ -5,29 +5,31 @@ let currentHometown = 0
 let currentUserId = 0
 let currentUserObj = {}
 let userLocations = {}
+let hometownCoords = {}
 
 
-// DOM QUERIES 
-const landmarkBar = document.querySelector(".landmark-bar")
-const loginBtn = document.querySelector("#login-button")
+//////////////////// DOM ELEMENTS /////////////////////////
 const titleBar = document.querySelector(".title-bar")
-
+const landmarkBar = document.querySelector(".landmark-bar")
 const hometownDiv = document.querySelector(".hometown-bar")
+const imgBox = document.querySelector(".image-box")
 
 const landmarkForm = document.querySelector(".landmark-form")
 const landmarkInfo = document.querySelector(".landmark-info")
-const imgBox = document.querySelector(".image-box")
 
+/* Log In / Sign Up */
 const signInBar2 = document.querySelector(".sign-in-2")
 const signInBar = document.querySelector(".sign-in-bar")
 const loginInput = document.querySelector("#login-input")
+const loginBtn = document.querySelector("#login-button")
 
 const signinTab = document.querySelector("#sign-in-tab")
 const signupTab = document.querySelector("#sign-up-tab")
+const loginInForm = document.querySelector("#login-form")
+const signupForm = document.querySelector("#new-user-form")
+//////////////////// END OF DOM ELEMENTS /////////////////////
 
-// DOM SLAPPAGE
-
-// FETCHES
+////////////////////////// FETCHES //////////////////////////
 
 // not in use but works 
 // function fetchUsers() {
@@ -56,17 +58,20 @@ function fetchAllLandmarks() {
         })
 }
 
+////////////////////// END OF FETCHES ////////////////////////
 
-// RENDER FUNCTIONS
+////////////////// RENDER FUNCITONS /////////////////////////
+
+/* SetCurrentUser -> Loads after logging in or signing in */
 function setCurrentUser(userObj) {
     signInBar.hidden = true
     // signInBar2.hidden = true
-
     landmarkBar.hidden = false
     imgBox.hidden = false
     landmarkForm.hidden = false
     hometownDiv.hidden = false
 
+    //Creating Buttons & Format Hometown Bar -> My Landmarks Button & All Landmarks 
     const button2 = document.createElement("button")
     button2.textContent = "All Landmarks"
     button2.classList.add("center")
@@ -83,20 +88,19 @@ function setCurrentUser(userObj) {
 
     const h1 = document.createElement("h1")
     h1.textContent = userObj.hometown
-
     hometownDiv.append(h1)
 
+    //Set Session State
     currentHometown = userObj.hometown
     currentUser = userObj.username
     currentUserId = userObj.id
     currentUserObj = userObj
 
-    fetchLandmarkCoord()
-    geocode(currentHometown)
-    // debugger
-    runApp(hometownCoords)
+    //Functions to run after creating page format/layout
+    fetchLandmarkCoord() //Fetch User Landmarks & Adds to Global UserLocations Array
 }
 
+/* renderLandmarks -> Displays Landmarks as a list on the Landmark Bar */
 function renderLandmarks(landmarksArray) {
     landmarkBar.innerHTML = ""
     const h5 = document.createElement("h5")
@@ -111,6 +115,7 @@ function renderLandmarks(landmarksArray) {
     })
 }
 
+/* renderLandmarkInfo-> Displays Landmarks Info at the bottom after clicking li landmark */
 function renderLandmarkInfo(id) {
     landmarkInfo.hidden = false
     const name = document.querySelector("#landmark-name")
@@ -120,7 +125,7 @@ function renderLandmarkInfo(id) {
     const img = document.createElement("img")
     img.classList.add("landmark-image")
 
-
+    //Fecth Specific Landmark to render info in appropiate places
     fetch(`http://localhost:3000/api/v1/landmarks/${id}`)
         .then(resp => resp.json())
         .then(landmarkObj => {
@@ -135,32 +140,38 @@ function renderLandmarkInfo(id) {
     imgBox.append(img)
 }
 
+/* renderAllLandmarks-> Displays all landmarks as a list on the Landmark Bar */
 function renderAllLandmarks(allLandmarks) {
 
     landmarkBar.innerHTML = ""
     const h5 = document.createElement("h5")
     h5.textContent = "All landmarks"
     landmarkBar.append(h5)
+    let allLandmarkCoords = {}
 
     allLandmarks.forEach(landmark => {
+        allLandmarkCoords[landmark.name] = landmark.coord
+
         const li = document.createElement("li")
         li.textContent = landmark.name
         li.dataset.id = landmark.id
         landmarkBar.append(li)
     })
-
+    addMarkers(allLandmarkCoords, hometownCoords)
 }
+////////////////////// END OF FETCHES ////////////////////////
 
 
+//////////////////// EVENT LISTENER /////////////////////////
 
-// EVENT LISTENERS
+/* CLick on LI Landmark in LandmarkBar -> Populate Landmark Info */
 landmarkBar.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
-        // debugger
         renderLandmarkInfo(parseInt(e.target.dataset.id))
     }
 })
 
+/* Submit on Landmark Form -> Add Landmark to DB & to LI List of My Landmarks */
 landmarkForm.addEventListener("submit", function (e) {
     e.preventDefault()
 
@@ -182,34 +193,22 @@ landmarkForm.addEventListener("submit", function (e) {
     })
         .then(resp => resp.json())
         .then(newLandmarkobj => {
-            // renderLandmarks(newLandmark)
-            // debugger
             if (newLandmarkobj.error) {
                 console.log(`${newLandmarkobj.exception}`)
-
             }
             else if (newLandmarkobj.name === newLandmark.name) {
-                // console.log("success")
-                // console.log(newLandmarkobj)
                 fetchUserLandmarks(currentUserId)
             }
         })
         .catch(error => console.log(error))
-    // debuggers
-
 })
 
-// v2
-const loginInForm = document.querySelector("#login-form")
-const signupForm = document.querySelector("#new-user-form")
+/* <---------- LOGIN & SIGNUP FORMS V2 ------------> */
+/* Click to LogIn/SignIn -> Fetch All Users to check for User & Set Session State andd Render User Landmarks */
 loginInForm.addEventListener("submit", function (e) {
     e.preventDefault()
-
-
-
     currentUser = loginInput.value
 
-    // debugger
     fetch("http://localhost:3000/api/v1/users")
         .then(resp => resp.json())
         .then(usersArr => {
@@ -218,13 +217,15 @@ loginInForm.addEventListener("submit", function (e) {
                     currentUser = user
                 }
             })
-            // debugger
-
             if (usersArr.find(user => user.username === currentUser.username)) {
                 setCurrentUser(currentUser)
-                renderLandmarks(currentUser.landmarks)
+                hometownCoords = geocodeHometown(currentHometown)
+
+                renderLandmarks(currentUserObj.landmarks)
+                // debugger
+
             }
-            else {
+            else { // Redirect to SignUp Form if User does not Exist
                 alert("Username does not exist!")
                 loginInForm.hidden = true
                 signupForm.hidden = false
@@ -238,9 +239,8 @@ loginInForm.addEventListener("submit", function (e) {
             }
         })
         .catch(error => console.log(error))
-
 })
-
+/* Toggle Between Forms By Clicking on Tab*/
 signinTab.addEventListener("click", function (e) {
     e.preventDefault()
 
@@ -270,16 +270,17 @@ signupTab.addEventListener("click", function (e) {
 
 })
 
+/* Submit to Create New User (SignUp) -> POST Fetch UserInput & Set Session State */
 signupForm.addEventListener("submit", function (e) {
     e.preventDefault()
 
-    // debugger
     const newUser = {
         username: e.target.username.value,
         hometown: e.target.hometown.value,
         img_url: e.target.img_url.value,
         bio: e.target.bio.value
     }
+
     fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
         headers: {
@@ -289,7 +290,6 @@ signupForm.addEventListener("submit", function (e) {
     })
         .then(resp => resp.json())
         .then(newUserObj => {
-            // debugger
             if (newUserObj) {
                 setCurrentUser(newUserObj)
             }
@@ -301,95 +301,28 @@ signupForm.addEventListener("submit", function (e) {
 
 
 })
-
+/* Click on My/All Landmark Buttons */
 hometownDiv.addEventListener("click", function (e) {
     e.preventDefault()
     if (e.target.id === "landmarks-button") {
         fetchAllLandmarks()
-        testerFunction()
     }
     else if (e.target.id === "my-button") {
         fetchUserLandmarks(currentUserId)
+        addMarkers(userLocations, hometownCoords)
     }
 })
-
-// function renderLocation () {
-//     for (let i = 0; i < userLocations.length; i++) {
-//         displayMap()
-//     }
-// }
 
 imgBox.addEventListener("click", function (e) {
     e.preventDefault()
-
     if (e.target.tagName === "li") {
-
     }
 })
+////////////////////// END OF EVENT LISTENER //////////////////
 
-//////////Map.js/////////////
-// let mapDiv = document.getElementById('map');
+///////////////////////// MAP JS /////////////////////////////
 
-function displayMap(coords) {
-    const mapOptions = {
-        center: coords,
-        zoom: 10
-    };
-    const mapDiv = document.getElementById('map');
-    return new google.maps.Map(mapDiv, mapOptions);
-}
-
-function runApp(coords) {
-    // debugger
-    console.log('Maps JS API loaded');
-    const map = displayMap(coords);
-    const markers = addMarkers(map)
-}
-const locations = {
-    operaHouse: { lat: 40.73062, lng: -73.98298 },
-    tarongaZoo: { lat: 40.75027, lng: -74.00013 },
-}
-
-// const userLocations = {{lat: 40.73062, lng: -73.98298 }, {lat: 40.73062, lng: -73.98298 }}
-
-
-
-function addMarkers(map) {
-    let userLocations = { stevenHouse: { lat: 40.585665, lng: -73.984832 } }
-
-    const markers = [];
-    // setCurrentUser(currentUserObj)
-    fetch(`http://localhost:3000/api/v1/landmarks`)
-        .then(resp => resp.json())
-        .then(landmarkArr => {
-            // debugger
-            landmarkArr.forEach(landmark => {
-                if (landmark.user.username === currentUser) {
-                    // debugger
-                    userLocations[landmark.name] = landmark.coord
-                }
-            })
-        })
-
-    for (const location in userLocations) {
-        const markerOptions = {
-            map: map,
-            position: userLocations[location],
-        }
-        const marker = new google.maps.Marker(markerOptions);
-        markers.push(marker);
-    }
-    // userLocations.forEach(landmark => {
-    //     const markerOptions = {
-    //         map: map,
-    //         position: userLocations[landmark],
-    //     }
-    //     const marker = new google.maps.Marker(markerOptions);
-    //     markers.push(marker);
-    // })
-    return markers;
-}
-
+/* Fetch Users Landmark & Adds to userLLocation Array */
 function fetchLandmarkCoord() {
     fetch(`http://localhost:3000/api/v1/landmarks`)
         .then(resp => resp.json())
@@ -403,16 +336,16 @@ function fetchLandmarkCoord() {
             })
         })
 }
-//////////End of Map.js//////
+////////////////////// END OF MAP.JS ///////////////////////////////
 
-///////////Geocode.js/////////
-
+///////////////////////// GEOCODE.JS ///////////////////////////////
 const searchBar = document.querySelector("#search-bar")
 
-let hometownCoords = {}
-let test5 = "2543 W 16th ST Brooklyn"
-geocode(test5)
-function geocode(location) {
+// let hometownCoords = geocode(currentHometown)
+// let test5 = currentHometown
+
+/* Gecode API -> Get coords from a Location(address) | Currently is coded to setting Global hometownCoords */
+function geocodeHometown(location) {
     axios.get("https://maps.googleapis.com/maps/api/geocode/json?", {
         params: {
             address: location,
@@ -420,32 +353,55 @@ function geocode(location) {
         }
     })
         .then(response => {
-            // console.log(response)
-            // debugger
             let result = response.data.results[0].geometry.location
             // createCoords(variable)
             // debugger
-            console.log(result)
             hometownCoords = result
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+let searchBarCoords = {}
+
+function geocodeSearchBar(location) {
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json?", {
+        params: {
+            address: location,
+            key: 'AIzaSyDCMZIf7DhtFbXH8I1fB6wb3dcXAB5mrOM'
+        }
+    })
+        .then(response => {
+            let result = response.data.results[0].geometry.location
+            // createCoords(variable)
+            debugger
+            return searchCoord[search] = result
+
         })
         .catch(error => {
             console.log(error)
         })
 }
 
-searchBar.addEventListener("submit", function (e) {
-    e.preventDefault()
-    // debugger
-    const search = e.target.search.value
-    geocode(search)
-})
 
-/////////End of Geocode.js/////
+/* Submit on Search Bar -> Passes input to Geocode Function */
+// searchBar.addEventListener("submit", function (e) {
+//     e.preventDefault()
+//     // debugger
+//     const search = e.target.search.value
+//     let searchCoord = {}
+//     searchCoord[search] = geocodeSearchBar(search)
+//     addMarkers(searchCoord)
+// })
 
-function testerFunction() {
-    const mapCenter = hometownCoords
+///////////////////////// END OF GEOCODE.JS ////////////////////////
+
+function addMarkers(markersArray, centerCoord) {
+    const mapCenter = centerCoord
     const map = displayMap();
     const markers = addMarkers(map)
+
     function displayMap() {
         const mapOptions = {
             center: mapCenter,
@@ -459,14 +415,62 @@ function testerFunction() {
     function addMarkers(map) {
         const markers = [];
         // setCurrentUser(currentUserObj)
-        for (const location in userLocations) {
+        for (const location in markersArray) {
+            // debugger
             const markerOptions = {
                 map: map,
-                position: userLocations[location],
+                position: markersArray[location],
             }
             const marker = new google.maps.Marker(markerOptions);
-            markers.push(marker);
+            // markers.push(marker);
+            marker.setMap(map)
         }
+        // debugger
         return markers;
     }
+
+}
+
+function runApp() {
+    console.log('Maps JS API loaded');
+    const map = displayMap(hometownCoords);
+}
+
+function displayMap(coord) {
+    const mapOptions = {
+        center: coord,
+        zoom: 14
+    };
+    const mapDiv = document.getElementById('map');
+    return new google.maps.Map(mapDiv, mapOptions);
+}
+
+function initMap() {
+    const newMap = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: hometownCoords,
+    });
+    const geocoder = new google.maps.Geocoder();
+    debugger
+    searchBar.addEventListener("submit", () => {
+        geocodeAddress(geocoder, newMap);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("search-input").value;
+
+    geocoder.geocode({ address: address }, (results, status) => {
+        debugger
+        if (status === "OK") {
+            // resultsMap.setCenter(results[0].geometry.location);
+            // new google.maps.Marker({
+            //     map: resultsMap,
+            //     position: results[0].geometry.location,
+            // });
+            console.log("Geocode success")
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
 }
